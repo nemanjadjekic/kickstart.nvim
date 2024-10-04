910302650ba11e0999f65aa84fc4bb4b30ede163
nemanjadjekic <ndjekic@proton.me>
2024-10-04 18:52:31 +0200

    Add shfmt

--- init.lua
+++ init.lua
@@ -708,6 +708,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        sh = { 'shfmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
